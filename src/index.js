791aab9d978262fb2e029f11d35131495b4d6f22
Glawian <krzysiek.liguda@gmail.com>
2020-02-04 20:07:36 +0100

    Login ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import style from "./css/index.scss"
-
+$(".showMenu").hide();
 $(document).ready(function () {
-    $(".showMenu").hide();
-    $("#navBar1").mouseenter(function() {
+    $("#navBar1, #showMenu1").hover(function () {
         $("#showMenu1").show();
-    });
-    $("#showMenu1, #navBar1").mouseleave(function() {
-        $("#showMenu1").hide();
-    });
+    },
+        function () {
+            $("#showMenu1").hide();
+        });
+    $("#navBar2, #showMenu2").hover(function () {
+        $("#showMenu2").show();
+    },
+        function () {
+            $("#showMenu2").hide();
+        });
+    $("#navBar3, #showMenu3").hover(function () {
+        $("#showMenu3").show();
+    },
+        function () {
+            $("#showMenu3").hide();
+        });
+    $("#navBar4, #showMenu4").hover(function () {
+        $("#showMenu4").show();
+    },
+        function () {
+            $("#showMenu4").hide();
+        });
 });
-
